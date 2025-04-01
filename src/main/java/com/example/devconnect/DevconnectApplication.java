8b1a1da18113601f9b3a5d0e15a627880122234b
Carlos <carloscuba014@gmail.com>
2025-04-01 09:31:34 -0400

    configure postgresql connection

--- src/main/java/com/example/devconnect/DevconnectApplication.java
+++ src/main/java/com/example/devconnect/DevconnectApplication.java
@@ -3,10 +3,14 @@ package com.example.devconnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DevconnectApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 		SpringApplication.run(DevconnectApplication.class, args);
 	}
 
